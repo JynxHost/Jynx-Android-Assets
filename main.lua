34acb6d7a92e938cd0ecb472803371b7fcd9a8e9
JynxHost <160290606+JynxHost@users.noreply.github.com>
2024-03-04 20:31:44 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,47 +3,7 @@ if jynxgetversion() ~= "V0.0.0.0" then
 	return
 end
 
-local env = getgenv()
-local JynxInternal = {}
-JynxInternal.writefile = writefile
-JynxInternal.appendfile = appendfile
-JynxInternal.readfile = readfile
-JynxInternal.loadfile = loadfile
-JynxInternal.isfile = isfile
-JynxInternal.isfolder = isfolder
-JynxInternal.makefolder = makefolder
-JynxInternal.delfolder = delfolder
-JynxInternal.delfile = delfile
-JynxInternal.listfiles = listfiles
-
---REDEFINITION
-local function hookAPI(name, new)
-	local old = env[name]
-	env[name] = function(...)
-		return new(old, ...)
-	end
-end
-hookAPI("writefile", function(old, path, data) return old("jynx/workspace/" .. path, data) end)
-hookAPI("appendfile", function(old, path, data) return old("jynx/workspace/" .. path, data) end)
-hookAPI("readfile", function(old, path) return old("jynx/workspace/" .. path) end)
-hookAPI("loadfile", function(old, path) return old("jynx/workspace/" .. path) end)
-hookAPI("isfile", function(old, path) assert(path and path ~= "", "Please provide a path.") return old("jynx/workspace/" .. path) end)
-hookAPI("isfolder", function(old, path) assert(path and path ~= "", "Please provide a path.") return old("jynx/workspace/" .. path) end)
-hookAPI("makefolder", function(old, path) assert(path and path ~= "", "Please provide a path.") return old("jynx/workspace/" .. path) end)
-hookAPI("delfolder", function(old, path) assert(path and path ~= "", "Please provide a path.") return old("jynx/workspace/" .. path) end)
-hookAPI("delfile", function(old, path) assert(path and path ~= "", "Please provide a path.") return old("jynx/workspace/" .. path) end)
-hookAPI("listfiles", function(old, path) return old("jynx/workspace/" .. path) end)
-
---before
-local jynxSetup = JynxInternal.isfolder("jynx")
-if not jynxSetup then
-	JynxInternal.makefolder("jynx")
-	JynxInternal.makefolder("jynx/autoexec")
-	JynxInternal.makefolder("jynx/scripts")
-	JynxInternal.makefolder("jynx/workspace")
-	JynxInternal.makefolder("jynx/bin")
-	JynxInternal.writefile("jynx/bin/version.txt", jynxgetversion())
-end
+local JynxInternal = loadstring(game:HttpGet("https://raw.githubusercontent.com/JynxHost/Jynx-Android-Assets/main/api.lua", true))()
 
 --##Variables##
 local _Hide = Instance.new("TextButton")
